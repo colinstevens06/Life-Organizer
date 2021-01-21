7b2fb793d892265b18a61894d0ce1b64b00b948a
Colin Stevens <colinstevens06@gmail.com>
2021-01-20 21:27:30 -0500

    findAll controller

---
+++ controllers/notesController.js
@@ -0,0 +1,13 @@
+const db = require("../models")
+if (process.env.NODE_ENV != "production") { const dotenv = require("dotenv/config") };
+
+
+// Defining methods for the notesController
+module.exports = {
+  findAll: function (req, res) {
+    db.Note
+      .find(req.query)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err))
+  }
+}
