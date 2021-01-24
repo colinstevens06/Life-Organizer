6285ba47002a78d366cbe95db3ea3c08446081b3
Colin Stevens <colinstevens06@gmail.com>
2021-01-24 12:50:54 -0500

    started button and working on the front end

---
+++ client/src/components/CreateNoteBtn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function CreateNoteBtn() {
+  return (
+    <div>
+      <div className="bnt_new-note_main">
+        Add New Note +
+      </div>
+    </div>
+  )
+}
