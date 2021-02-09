9448b76483acc973e893d5de05668dae610add69
Colin Stevens <colinstevens06@gmail.com>
2021-02-09 17:42:54 -0500

    added catch in case there are no notes in the DB

--- client/src/components/main/DataRowContainer.js
+++ client/src/components/main/DataRowContainer.js
@@ -5,15 +5,20 @@ export default function DataRowContainer(props) {
   return (
     <div>
       {props.allNotes &&
-        props.allNotes.map(note => (
-          <DataRow
-            key={note._id}
-            id={note._id}
-            name={note.name}
-            category={note.category}
-            date={note.lastUpdated.slice(0, 10)}
-          />
-        ))
+        (props.allNotes >= 1) ? (
+          props.allNotes.map(note => (
+            <DataRow
+              key={note._id}
+              id={note._id}
+              name={note.name}
+              category={note.category}
+              date={note.lastUpdated.slice(0, 10)}
+            />
+          ))
+
+        ) : (
+          <div>Start creating some notes using the button above</div>
+        )
       }
     </div>
   )
