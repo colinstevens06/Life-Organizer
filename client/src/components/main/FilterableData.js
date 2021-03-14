3999fdf0e4498a77aa7a006f6dc8bb15d745ff7e
Colin Stevens <colinstevens06@gmail.com>
2021-03-13 21:48:32 -0500

    added sorting arrows for landing page data

--- client/src/components/main/FilterableData.js
+++ client/src/components/main/FilterableData.js
@@ -1,5 +1,8 @@
 import React from "react";
 import DataRow from "./DataRow";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSortDown, faSortUp } from '@fortawesome/free-solid-svg-icons'
+
 
 function FilterableData(props) {
 
@@ -7,9 +10,22 @@ function FilterableData(props) {
     <div className="container_notes-list">
       <div className="header_notes-list">Your Notes</div>
       <div className="row_notes-list sortable-headers__border">
-        <div className="header__sortable-lists" onClick={() => props.handleSortTypeValueChange("name")}>Name</div>
-        <div className="header__sortable-lists" onClick={() => props.handleSortTypeValueChange("date")}>Last Modified</div>
-        <div className="header__sortable-lists" onClick={() => props.handleSortTypeValueChange("category")}>Category</div>
+        <div className="header__sortable-lists" onClick={() => props.handleSortTypeValueChange("name")}>Name&nbsp;
+        {props.sortClass === "name-ascending" && <FontAwesomeIcon icon={faSortUp} title="Sort Up" />}
+          {props.sortClass === "name-descending" && <FontAwesomeIcon icon={faSortDown} title="Sort Up" />}
+
+
+        </div>
+        <div className="header__sortable-lists" onClick={() => props.handleSortTypeValueChange("date")}>Last Modified&nbsp;
+          {props.sortClass === "date-descending" && <FontAwesomeIcon icon={faSortDown} title="Sort Up" />}
+          {props.sortClass === "date-ascending" && <FontAwesomeIcon icon={faSortUp} title="Sort Up" />}
+
+        </div>
+        <div className="header__sortable-lists" onClick={() => props.handleSortTypeValueChange("category")}>Category&nbsp;
+        
+          {props.sortClass === "category-descending" && <FontAwesomeIcon icon={faSortDown} title="Sort Up" />}
+          {props.sortClass === "category-ascending" && <FontAwesomeIcon icon={faSortUp} title="Sort Up" />}
+        </div>
       </div>
 
       {
